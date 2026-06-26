d9d329d666af7b620ce2dbbb2605fb764ed428ee
Joshua Poro <jporo@kim.church>
2026-06-25 21:18:20 -0500

    add PWA manifest and service worker

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'kim-v1';
+const STATIC = [
+  '/',
+  '/give',
+  '/vision',
+  '/checkin',
+  '/pickup',
+  '/generosity',
+  '/kim-logo.png',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&family=DM+Sans:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
